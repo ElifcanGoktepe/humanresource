020a96e9fe6d789d92f3ba14ed4d2f983a341c5c
ElifcanGoktepe <elifcangoktepe@gmail.com>
2025-05-25 18:40:44 +0300

    manager rolleri ile ilgili ayarlamalar yapıldı

--- src/main/java/com/project/humanresource/service/EmployeeService.java
+++ src/main/java/com/project/humanresource/service/EmployeeService.java
@@ -8,6 +8,7 @@ import com.project.humanresource.exception.ErrorType;
 import com.project.humanresource.exception.HumanResourceException;
 import com.project.humanresource.repository.*;
 import com.project.humanresource.utility.UserStatus;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -34,7 +35,12 @@ public class EmployeeService {
         employeeRepository.save(employee);
     }
 
-    public void addEmployeeForManager(AddEmployeeForRoleRequirementDto dto) { //manager tarafından eklenen employee
+    public void addEmployeeForManager(AddEmployeeForRoleRequirementDto dto,  HttpServletRequest request) { //manager tarafından eklenen employee
+        Long managerId = (Long) request.getAttribute("userId");
+
+        if (managerId == null) {
+            throw new IllegalStateException("Manager ID not found in request.");
+        }
         Employee employee = Employee.builder()
                 .firstName(dto.firstName())
                 .lastName(dto.lastName())
