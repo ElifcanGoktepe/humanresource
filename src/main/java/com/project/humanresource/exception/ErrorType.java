871942333e4f778597b4f7d5ba32fbaef42d1c9c
ElifcanGoktepe <elifcangoktepe@gmail.com>
2025-06-04 19:37:03 +0300

    ErrorType güncellendi

--- src/main/java/com/project/humanresource/exception/ErrorType.java
+++ src/main/java/com/project/humanresource/exception/ErrorType.java
@@ -42,7 +42,7 @@ public enum ErrorType {
     INVALID_FILE_TYPE(4016, "Invalid file type. Only JPG, JPEG, PNG, GIF are allowed.", BAD_REQUEST),
     INVALID_FILE_NAME(4017, "Invalid file name.", BAD_REQUEST),
     INVALID_FILE_EXTENSION(4018, "Invalid file extension.", BAD_REQUEST);
-
+    // yenileri eklendi
 
 
 
